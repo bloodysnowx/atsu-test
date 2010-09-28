421d8e702785c843631cfcbe1484099e4e4abdc3
bloodysnow <genius@tcct.zaq.ne.jp>
2010-09-28 15:48:44 +0000

    ゲーム盤の初期化処理作成 ゲーム盤の終了処理作成
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@5 02286809-4c3e-56e6-4757-eef48aeb7bab

--- 01_puzzle/01_puzzle_2/main.cpp
+++ 01_puzzle/01_puzzle_2/main.cpp
@@ -3,7 +3,38 @@
 
 using namespace std;
 
+char ** now_position;
+
 int main()
 {
+	newPosition();
+	initializePosition();
+
+	exitPosition();
 	return 0;
+}
+
+void newPosition()
+{
+	now_position = new char*[GAME_SIZE_Y];
+
+	for(int i=0; i < GAME_SIZE_Y; ++i)
+		now_position[i] = new char[GAME_SIZE_X];
+}
+
+void initializePosition()
+{
+	for(int i=0; i < GAME_SIZE_Y; ++i)
+	{
+		for(int j=0; j < GAME_SIZE_X; ++j)
+			now_position[i][j] = INITIAL_POSITION[i][j];
+	}
+}
+
+void exitPosition()
+{
+	for(int i=0; i < GAME_SIZE_Y; ++i)
+		delete[] now_position[i];
+
+	delete[] now_position;
 }
--- 01_puzzle/01_puzzle_2/main.h
+++ 01_puzzle/01_puzzle_2/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+void newPosition();
+void initializePosition();
+void exitPosition();
+
 static const int GAME_SIZE_X = 9;
 static const int GAME_SIZE_Y = 5;
 
@@ -12,7 +16,7 @@ static const int GAME_SIZE_Y = 5;
  p:プレイヤー
  P:プレイヤー+ゴール
 */
-const char INITIAL_GAME[GAME_SIZE_Y][GAME_SIZE_X] = 
+const char INITIAL_POSITION[GAME_SIZE_Y][GAME_SIZE_X] = 
 {
 	"########",
 	"# .. p #",
