31bb67cea4dfced4bb17a18b5624f4debf84f759
bloodysnow <genius@tcct.zaq.ne.jp>
2010-09-29 06:03:27 +0000

    メインループ作成 現在地の初期化 入力受付の実装 移動処理の実装
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@7 02286809-4c3e-56e6-4757-eef48aeb7bab

--- 01_puzzle/01_puzzle_2/main.cpp
+++ 01_puzzle/01_puzzle_2/main.cpp
@@ -5,11 +5,21 @@ using namespace std;
 
 char ** now_position;
 
+int now_x, now_y;
+int moving_x, moving_y;
+int pushing_x, pushing_y;
+
 int main()
 {
 	newPosition();
 	initializePosition();
-	draw();
+
+	while(true)
+	{
+		draw();
+		getInput();
+		updateGame();
+	}
 
 	exitPosition();
 	return 0;
@@ -30,6 +40,9 @@ void initializePosition()
 		for(int j=0; j < GAME_SIZE_X; ++j)
 			now_position[i][j] = INITIAL_POSITION[i][j];
 	}
+
+	now_x = INITIAL_X;
+	now_y = INITIAL_Y;
 }
 
 void exitPosition()
@@ -46,4 +59,169 @@ void draw()
 		cout << now_position[i] << endl;
 
 	cout << "a:left s:right w:up z:down. command? ";
+}
+
+void getInput()
+{
+	char c;
+	cin >> c;
+	
+	switch (c)
+	{
+	case 'a':
+		moving_x = now_x - 1;
+		moving_y = now_y;
+		pushing_x = now_x - 2;
+		pushing_y = now_y;
+		break;
+	case 's':
+		moving_x = now_x + 1;
+		moving_y = now_y;
+		pushing_x = now_x + 2;
+		pushing_y = now_y;
+		break;
+	case 'w':
+		moving_x = now_x;
+		moving_y = now_y - 1;
+		pushing_x = now_x;
+		pushing_y = now_y - 2;
+		break;
+	case 'z':
+		moving_x = now_x;
+		moving_y = now_y + 1;
+		pushing_x = now_x;
+		pushing_y = now_y + 2;
+		break;
+	default:
+		moving_x = now_x;
+		moving_y = now_y;
+		pushing_x = now_x;
+		pushing_y = now_y;
+		break;
+	}
+}
+
+void updateGame()
+{
+	// 移動しなかった場合
+	if(now_x == moving_x && now_y == moving_y)
+		return;
+
+	switch (now_position[moving_y][moving_x])
+	{
+		// 壁にぶつかった場合
+	case '#':
+		break;
+		// スペースに移動した場合
+	case ' ':
+		// ゴールに重なっていた場合
+		if(now_position[now_y][now_x] == 'P')
+			now_position[now_y][now_x] = '.';
+		else
+			now_position[now_y][now_x] = ' ';
+
+		now_x = moving_x;
+		now_y = moving_y;
+
+		now_position[now_y][now_x] = 'p';
+		break;
+
+		// ゴールに移動
+	case '.':
+		// ゴールに重なっていた場合
+		if(now_position[now_y][now_x] == 'P')
+			now_position[now_y][now_x] = '.';
+		else
+			now_position[now_y][now_x] = ' ';
+
+		now_x = moving_x;
+		now_y = moving_y;
+
+		now_position[now_y][now_x] = 'P';
+		break;
+
+		// 荷物を押す
+	case 'o':
+		switch (now_position[pushing_y][pushing_x])
+		{
+			// ぶつかった
+		case 'o':
+		case 'O':
+		case '#':
+			break;
+			// 押せた
+		case ' ':
+			if(now_position[now_y][now_x] == 'P')
+				now_position[now_y][now_x] = '.';
+			else
+				now_position[now_y][now_x] = ' ';
+
+			now_position[moving_y][moving_x] = 'p';
+			now_position[pushing_y][pushing_x] = 'o';
+			
+			now_x = moving_x;
+			now_y = moving_y;
+			break;
+			// ゴールに到着
+		case '.':
+			if(now_position[now_y][now_x] == 'P')
+				now_position[now_y][now_x] = '.';
+			else
+				now_position[now_y][now_x] = ' ';
+
+			now_position[moving_y][moving_x] = 'p';
+			now_position[pushing_y][pushing_x] = 'O';
+			
+			now_x = moving_x;
+			now_y = moving_y;
+			break;
+		default:
+			break;
+		}
+		break;
+
+		// ゴールにかさなった荷物を押す
+	case 'O':
+		switch (now_position[pushing_y][pushing_x])
+		{
+			// ぶつかった
+		case 'o':
+		case 'O':
+		case '#':
+			break;
+			// 押せた
+		case ' ':
+			if(now_position[now_y][now_x] == 'P')
+				now_position[now_y][now_x] = '.';
+			else
+				now_position[now_y][now_x] = ' ';
+
+			now_position[moving_y][moving_x] = 'P';
+			now_position[pushing_y][pushing_x] = 'o';
+			
+			now_x = moving_x;
+			now_y = moving_y;
+			break;
+			// ゴールに到着
+		case '.':
+			if(now_position[now_y][now_x] == 'P')
+				now_position[now_y][now_x] = '.';
+			else
+				now_position[now_y][now_x] = ' ';
+
+			now_position[moving_y][moving_x] = 'P';
+			now_position[pushing_y][pushing_x] = 'O';
+			
+			now_x = moving_x;
+			now_y = moving_y;
+			break;
+		default:
+			break;
+		}
+		break;
+
+	default:
+		break;
+	}
+
 }
--- 01_puzzle/01_puzzle_2/main.h
+++ 01_puzzle/01_puzzle_2/main.h
@@ -5,6 +5,8 @@ void newPosition();
 void initializePosition();
 void exitPosition();
 void draw();
+void getInput();
+void updateGame();
 
 static const int GAME_SIZE_X = 9;
 static const int GAME_SIZE_Y = 5;
@@ -25,5 +27,8 @@ const char INITIAL_POSITION[GAME_SIZE_Y][GAME_SIZE_X] =
 	"#      #",
 	"########"
 };
+
+static const int INITIAL_X = 5;
+static const int INITIAL_Y = 1;
 
 #endif /* _MAIN_H_ */
