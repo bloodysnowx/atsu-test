c219eb86f79baf582cf773884f90b3dc7ab7ab6d
bloodysnow <genius@tcct.zaq.ne.jp>
2011-03-11 08:27:32 +0000

    Cardクラス実装 Hand_5とHand_7クラスの雛形生成
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@47 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/Holdem/HoldemLib/Card.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HoldemLib
+{
+    /// <summary>
+    /// カード(スートとランク)
+    /// </summary>
+    class Card
+    {
+        /// <summary>
+        /// スート
+        /// </summary>
+        public HAND_SUIT suit { private set; get; }
+        /// <summary>
+        /// ランク
+        /// </summary>
+        public uint rank { private set; get; }
+
+        /// <summary>
+        /// スートとランクを初期化する
+        /// </summary>
+        /// <param name="suit"></param>
+        /// <param name="rank"></param>
+        public Card(HAND_SUIT suit, uint rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+
+        /// <summary>
+        /// スートとランクを初期化する
+        /// </summary>
+        /// <param name="card_str"></param>
+        public Card(string card_str)
+        {
+            switch (card_str[0])
+            {
+                case '2': rank =  2; break;
+                case '3': rank =  3; break;
+                case '4': rank =  4; break;
+                case '5': rank =  5; break;
+                case '6': rank =  6; break;
+                case '7': rank =  7; break;
+                case '8': rank =  8; break;
+                case '9': rank =  9; break;
+                case 'T':
+                case 't': rank = 10; break;
+                case 'J':
+                case 'j': rank = 11; break;
+                case 'Q':
+                case 'q': rank = 12; break;
+                case 'K':
+                case 'k': rank = 13; break;
+                case '1':
+                case 'A':
+                case 'a': rank = 14; break;
+                default : rank =  0;
+                    throw new ArgumentException("unknown rank", "card_str");
+            }
+
+            switch (card_str[1])
+            {
+                case 's': suit = HAND_SUIT.SPADES;		break;
+                case 'd': suit = HAND_SUIT.DIAMONDS;	break;
+                case 'h': suit = HAND_SUIT.HEARTS;		break;
+                case 'c': suit = HAND_SUIT.CLUBS;		break;
+                default : suit = HAND_SUIT.UNKNOWN;
+                    throw new ArgumentException("unknown suit", "card_str");
+	        }
+        }
+    }
+}
---
+++ poker/Holdem/HoldemLib/enum.cs
@@ -0,0 +1,24 @@
+﻿namespace HoldemLib
+{
+    enum HAND_SUIT
+    {
+        UNKNOWN = 0,
+        SPADES = 1, // s
+        DIAMONDS, // d
+        HEARTS, // h
+        CLUBS // c
+    };
+
+    enum HAND_CLASS
+    {
+        NO_PAIR,
+        PAIR,
+        TWO_PAIR,
+        THREE_OF_A_KIND,
+        STRAIGHT,
+        FLUSH,
+        FULL_HOUSE,
+        FOUR_OF_A_KIND,
+        STRAIGHT_FLUSH
+    };
+}
