c831a9d582dc0d6c546c0c07534ae9ebcf78e20e
bloodysnow <genius@tcct.zaq.ne.jp>
2011-04-20 06:54:46 +0000

    HUTrainerを新規作成開始
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@75 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/HUTrainer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HUTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
