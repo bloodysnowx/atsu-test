01975584132bdcacf91c93a70de54fbaf3638db2
bloodysnow <genius@tcct.zaq.ne.jp>
2011-04-23 03:27:15 +0000

    note文字列が不正な場合に対応
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@77 02286809-4c3e-56e6-4757-eef48aeb7bab

--- poker/PTRtoNote/FormMain.cs
+++ poker/PTRtoNote/FormMain.cs
@@ -157,12 +157,20 @@ namespace PTRtoNote
                         xmlReader.MoveToAttribute("label");
                         string label = xmlReader.Value;
                         // noteの内容を読み込む
-                        string note_str = xmlReader.ReadString();
+                        string note_str = "";
+                        try
+                        {
+                            note_str = xmlReader.ReadString();
+                        }
+                        catch (Exception ex)
+                        {
+                            if (logger.IsErrorEnabled) logger.Error(player_name + "'s note is broken.", ex);
+                        }
 
                         PTRData data = new PTRData(player_name);
 
                         // 取得指示があった場合
-                        if (note_str == "a" || (note_str[0] == 'a' && note_str[1] == '\n'))
+                        if (note_str.Length < 1 || note_str == "a" || (note_str[0] == 'a' && note_str[1] == '\n'))
                         {
                             // 取得実行
                             data = GetPTR(player_name);
@@ -187,7 +195,7 @@ namespace PTRtoNote
                             if (data.GetDate < System.DateTime.Today - new System.TimeSpan(Properties.Settings.Default.ReacquisitionSpanDays, 0, 0, 0))
                             {
                                 PTRData new_data = GetPTR(player_name);
-                                if(new_data != null && new_data.Hands >= data.Hands)
+                                if (new_data != null && new_data.Hands >= data.Hands)
                                 {
                                     data = new_data;
                                     note_str = data.GetNoteString() + '\n' + note_str;
@@ -322,9 +330,9 @@ namespace PTRtoNote
         {
             string label = "5";
             if (data.BB_100 > Properties.Settings.Default.Label_0_Min) label = "0";
-            else if (data.Hands < Properties.Settings.Default.Label_6_Hand_Max) label = "6";
             else if (data.BB_100 > Properties.Settings.Default.Label_1_Min) label = "1";
             else if (data.BB_100 > Properties.Settings.Default.Label_2_Min) label = "2";
+            else if (data.Hands < Properties.Settings.Default.Label_6_Hand_Max) label = "6";
             else if (data.BB_100 > Properties.Settings.Default.Label_3_Min) label = "3";
             else if (data.BB_100 > Properties.Settings.Default.Label_4_Min) label = "4";
 
