b71d9dfb470c98f58e578d6f68f2ecfcf3c7497c
bloodysnow <genius@tcct.zaq.ne.jp>
2011-11-08 09:12:42 +0000

    WPFに移植開始 クラス設計を開始
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@130 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/PTRtoNote2/PTRtoNote2/IConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTRtoNote2
+{
+    public interface IConnection
+    {
+        string[] Users
+        {
+            get;
+            set;
+        }
+
+        string[] Passwords
+        {
+            get;
+            set;
+        }
+
+        PTRData GetPTRData(string player_name);
+    }
+}
