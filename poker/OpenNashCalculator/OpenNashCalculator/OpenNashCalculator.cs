765df48143e82f4fe9e27b9f8d769a954556dbf1
bloodysnow <genius@tcct.zaq.ne.jp>
2012-04-19 02:21:01 +0000

    Modelの分離開始
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@142 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/OpenNashCalculator/OpenNashCalculator/OpenNashCalculator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenNashCalculator
+{
+    public class OpenNashCalculator
+    {
+        public int[] BB
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int[] SB
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int[] Ante
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int CurrentBB
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int CurrentSB
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int CurrentAnte
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Structure
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
+        public void Open()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Open(int BB, int SB, int Ante, string Structure, int[] chips)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
