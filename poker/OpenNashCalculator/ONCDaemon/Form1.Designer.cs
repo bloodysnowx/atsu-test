26430d3e43a1e2d62b678be36c4346c7ec2876a6
bloodysnow <genius@tcct.zaq.ne.jp>
2012-05-22 02:28:36 +0000

    ハンドヒストリ監視デーモンを作成開始
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@176 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/OpenNashCalculator/ONCDaemon/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ONCDaemon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HandHistoryFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(13, 13);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog HandHistoryFolderBrowserDialog;
+        private System.Windows.Forms.Button buttonOpen;
+
+    }
+}
+
---
+++ poker/OpenNashCalculator/ONCDaemon/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ONCDaemon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HandHistoryFolderBrowserDialog.SelectedPath = Properties.Settings.Default.HandHistoryFolder;
+            System.Diagnostics.Process.Start("./OpenNashCalculator.exe", "\"D:\\src\\atsu-test\\poker\\HH20120418 T546368001 No Limit Hold'em $3 + $0.30.txt\"");
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (HandHistoryFolderBrowserDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+        }
+    }
+}
