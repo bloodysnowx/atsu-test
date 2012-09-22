464ed61a3824bcb39a0ab517328b3c8cd04e0068
bloodysnow <genius@tcct.zaq.ne.jp>
2012-09-22 07:34:43 +0000

    ONCDaemonのアプリ名を変更 リソースを日本語化
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@195 02286809-4c3e-56e6-4757-eef48aeb7bab

--- poker/OpenNashCalculator/ONCDaemon/Form1.Designer.cs
+++ poker/OpenNashCalculator/ONCDaemon/Form1.Designer.cs
@@ -28,15 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
             this.Name = "Form1";
-            this.Text = "ONCDaemon";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
