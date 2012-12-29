dd090ddbb0569251078849ce8f46a1ff7b62d23a
bloodysnow <genius@tcct.zaq.ne.jp>
2012-12-29 05:22:49 +0000

    UIとテーブル情報を分離 PokerStarsのハンド読み込みをリファクタリング(FullTilt流用のために)
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@203 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/OpenNashCalculator/OpenNashCalculator/TableData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenNashCalculator
+{
+    class TableData
+    {
+        public int BB { get; set; }
+        public int SB { get; set; }
+        public int Ante { get; set; }
+        public string Structure { get; set; }
+        public int hero_pos { get; set; }
+        public string[] positions { get; set; }
+        public string[] playerNames { get; set; }
+        public int[] chips { get; set; }
+        public int buttonPos { get; set; }
+    }
+}
