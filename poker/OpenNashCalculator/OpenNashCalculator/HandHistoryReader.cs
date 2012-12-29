bb3f8fb9c051c6abd0b6855c09ca49d4a88ede8a
bloodysnow <genius@tcct.zaq.ne.jp>
2012-12-29 05:21:21 +0000

    PokerStars, FullTiltの切り替えに対応するために、インターフェース定義
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@202 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/OpenNashCalculator/OpenNashCalculator/HandHistoryReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenNashCalculator
+{
+    interface HandHistoryReader
+    {
+        /// <summary>指定したファイルの指定したハンド数前のハンドヒストリーを読み込む</summary>
+        /// <param name="fileName">ファイル名</param>
+        /// <param name="backNum">何ハンド前か？</param>
+        /// <returns>読み込んだ結果のテーブル状況(NashCalculateするために用いる)</returns>
+        TableData read(string fileName, int backNum);
+    }
+}
