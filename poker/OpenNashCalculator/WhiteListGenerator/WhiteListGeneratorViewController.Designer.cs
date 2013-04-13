f0ff327f22b98832054576d59067db85dbb2e4f5
bloodysnow <genius@tcct.zaq.ne.jp>
2013-04-13 05:39:47 +0000

    WhiteListGeneratorを作成開始
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@220 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/OpenNashCalculator/WhiteListGenerator/WhiteListGeneratorViewController.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WhiteListGeneratorUI
+{
+    partial class WhiteListGeneratorViewController
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.userNameTextBox.Location = new System.Drawing.Point(15, 29);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(192, 19);
+            this.userNameTextBox.TabIndex = 1;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateButton.Location = new System.Drawing.Point(15, 54);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(192, 23);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // WhiteListGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 89);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "WhiteListGenerator";
+            this.Text = "WhiteListGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+
+    }
+}
+
---
+++ poker/OpenNashCalculator/WhiteListGenerator/WhiteListGeneratorViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WhiteListGeneratorUI
+{
+    public partial class WhiteListGeneratorViewController : Form
+    {
+        public WhiteListGeneratorViewController()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            if (userNameTextBox.Text.Length == 0) return;
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+            
+        }
+    }
+}
