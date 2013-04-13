d6f5a46739b19e37f56481b2c71518bcb910bc95
bloodysnow <genius@tcct.zaq.ne.jp>
2013-04-13 07:43:33 +0000

    User認証のライブラリを追加
    git-svn-id: https://atsu-test.googlecode.com/svn/trunk@222 02286809-4c3e-56e6-4757-eef48aeb7bab

---
+++ poker/OpenNashCalculator/UserValidatorLib/UserNameEncrypter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserValidatorLib
+{
+    public class UserNameEncrypter
+    {
+        public String encryptFromUserName(String userName)
+        {
+            byte[] originalData = System.Text.Encoding.UTF8.GetBytes("chiyuki" + userName + "血で染まる雪");
+            var md5gen = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] encodedData = md5gen.ComputeHash(originalData);
+            var result = new System.Text.StringBuilder();
+            foreach(byte b in encodedData)
+                result.Append(b.ToString("x2"));
+            return result.ToString();
+        }
+    }
+}
---
+++ poker/OpenNashCalculator/UserValidatorLib/UserValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserValidatorLib
+{
+    public class UserValidator
+    {
+        UserNameEncrypter encrypter;
+
+        public UserValidator()
+        {
+            encrypter = new UserNameEncrypter();
+        }
+
+        public bool validate(String userName, String encryptedString)
+        {
+            String encryptedUserName = encrypter.encryptFromUserName(userName);
+            return encryptedUserName.Equals(encryptedString);
+        }
+    }
+}
