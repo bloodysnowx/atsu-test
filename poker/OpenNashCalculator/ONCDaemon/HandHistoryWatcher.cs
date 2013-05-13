6006973cc3ad8a7ff5aa7a35cfb0a2618c0c38af
bloodysnowx <genius@tcct.zaq.ne.jp>
2013-05-13 18:11:16 +0900

    指定したフォルダ以下に存在するすべてのフォルダを監視するようにした

--- poker/OpenNashCalculator/ONCDaemon/HandHistoryWatcher.cs
+++ poker/OpenNashCalculator/ONCDaemon/HandHistoryWatcher.cs
@@ -13,6 +13,25 @@ namespace ONCDaemon
 
         List<System.IO.FileSystemWatcher> watchers = new List<System.IO.FileSystemWatcher>();
 
+        private string[] getAllDirs(String path)
+        {
+            string[] subDirs = System.IO.Directory.GetDirectories(path);
+            string[] allDirs = new string[subDirs.Length + 1];
+            Array.Copy(subDirs, allDirs, subDirs.Length);
+            allDirs[subDirs.Length] = path;
+            return allDirs;
+        }
+
+        private IEnumerable<System.IO.FileSystemWatcher> createWatchers(String path, System.ComponentModel.ISynchronizeInvoke synchroObj)
+        {
+            string[] allDirs = getAllDirs(path);
+
+            foreach (string dirPath in allDirs)
+            {
+                yield return createWatcher(dirPath, synchroObj);
+            }
+        }
+
         private System.IO.FileSystemWatcher createWatcher(String path, System.ComponentModel.ISynchronizeInvoke synchroObj)
         {
             var watcher = new System.IO.FileSystemWatcher();
@@ -26,12 +45,18 @@ namespace ONCDaemon
             return watcher;
         }
 
+        private void invokeEventForExistingFiles(string path)
+        {
+            foreach (var file in System.IO.Directory.GetFiles(path).Where(file => System.IO.File.GetLastWriteTime(file) > DateTime.Now.AddMinutes(-10)))
+                watcher_Changed(this, new System.IO.FileSystemEventArgs(System.IO.WatcherChangeTypes.Created, path, System.IO.Path.GetFileName(file)));
+        }
+
         public void addFolder(String path, System.ComponentModel.ISynchronizeInvoke synchroObj)
         {
-            var watcher = createWatcher(path, synchroObj);
-            foreach(var file in System.IO.Directory.GetFiles(path).Where(file => System.IO.File.GetLastWriteTime(file) > DateTime.Now.AddMinutes(-10)))
-                watcher_Changed(this, new System.IO.FileSystemEventArgs(System.IO.WatcherChangeTypes.Created, path, System.IO.Path.GetFileName(file)));
-            watchers.Add(watcher);
+            IEnumerable<System.IO.FileSystemWatcher> newWatchers = createWatchers(path, synchroObj);
+            string[] allDirs = getAllDirs(path);
+            foreach (string dir in allDirs) invokeEventForExistingFiles(dir);
+            watchers.AddRange(newWatchers);
         }
 
         private void watcher_Changed(System.Object source, System.IO.FileSystemEventArgs e)
