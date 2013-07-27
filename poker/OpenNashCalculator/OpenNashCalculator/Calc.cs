c460f7bdf7e519e976aa226aa2dcc346653929ec
bloodysnowx <genius@tcct.zaq.ne.jp>
2013-07-27 22:26:12 +0900

    前回のコミット漏れ

--- poker/OpenNashCalculator/OpenNashCalculator/Calc.cs
+++ poker/OpenNashCalculator/OpenNashCalculator/Calc.cs
@@ -9,6 +9,8 @@ using System.Windows.Forms;
 using System.Web;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Xml;
+using System.Data.SQLite;
 
 using System.Text.RegularExpressions;
 
@@ -16,11 +18,185 @@ namespace OpenNashCalculator
 {
     public partial class OpenNashCalculatorViewController : Form
     {
+        InputXMLCreator creator = new InputXMLCreator();
+        XmlDocument resultXML;
+        System.Diagnostics.Process currentProcess;
+
         private void Calc()
         {
             if (chipTextBoxes.Count(x => x.Text != string.Empty && System.Convert.ToInt32(x.Text) > 0) <= 1) return;
-
+            textBoxStructure.Text = textBoxStructure.Text.Replace('+', ',').Replace(' ', ',');
             this.buttonCalc.Enabled = false;
+            if (CanCalcByCLI()) CalcByCLI();
+            else CalcByWeb();
+        }
+
+        private bool CanCalcByCLI()
+        {
+            return System.IO.File.Exists("net.holdemresources.cli.jar");
+        }
+
+        private string getSQLKey()
+        {
+            return currentTableData.Structure + currentTableData.BB + currentTableData.SB + currentTableData.Ante + currentTableData.stacks;
+        }
+
+        private string getInputXMLname()
+        {
+            string tourneyID = this.reader.getTourneyID(openHandHistoryDialog.FileName);
+            return "input_" + tourneyID + ".xml";
+        }
+
+        private string getOutputXMLname()
+        {
+            string tourneyID = this.reader.getTourneyID(openHandHistoryDialog.FileName);
+            return "output_" + tourneyID + ".xml";
+        }
+
+        private void CalcByCLI()
+        {
+            foreach (TextBox rangeTextBox in rangeTextBoxes) rangeTextBox.Clear();
+            if (currentTableData == null) currentTableData = new TableData();
+            currentTableData.Structure = textBoxStructure.Text.Trim();
+            currentTableData.BB = System.Convert.ToInt32(textBoxBB.Text.Trim());
+            currentTableData.SB = currentTableData.BB / 2;
+            currentTableData.Ante = System.Convert.ToInt32(textBoxAnte.Text.Trim());
+            currentTableData.stacks = string.Empty;
+            for (int i = 1; i < 10; ++i)
+            {
+                if (chipTextBoxes[(bb_pos + i) % 9].Text.Trim() != string.Empty)
+                {
+                    if (currentTableData.stacks.Length > 0) currentTableData.stacks += ",";
+                    int chip = System.Convert.ToInt32(chipTextBoxes[(bb_pos + i) % 9].Text.Trim());
+                    chip = (int)(Math.Round((double)chip / 10.0) * 10);
+                    currentTableData.stacks += chip.ToString();
+                }
+            }
+
+            using (var conn = new SQLiteConnection("Data Source=calc.db"))
+            {
+                conn.Open();
+                using (var selectCommand = conn.CreateCommand())
+                {
+                    selectCommand.CommandText = "select data from CalculatedData where id = @id";
+                    selectCommand.Parameters.Add("id", System.Data.DbType.String);
+                    selectCommand.Prepare();
+                    selectCommand.Parameters["id"].Value = getSQLKey();
+
+                    using (var reader = selectCommand.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string resultData = reader.GetString(0);
+                            if (resultData.Length < 30)
+                            {
+                                using (var deleteCommand = conn.CreateCommand())
+                                {
+                                    deleteCommand.CommandText = "delete from CalculatedData where id = @id";
+                                    deleteCommand.Parameters.Add("id", System.Data.DbType.String);
+                                    deleteCommand.Prepare();
+                                    deleteCommand.Parameters["id"].Value = getSQLKey();
+                                    deleteCommand.ExecuteNonQuery();
+                                    CalcByCLI();
+                                }
+                            }
+                            else
+                            {
+                                resultXML = new XmlDocument();
+                                resultXML.LoadXml(resultData);
+                                readFromXML();
+                            }
+                        }
+                        else
+                        {
+                            var inputXML = creator.create(currentTableData);
+                            inputXML.Save(getInputXMLname());
+
+                            if (currentProcess != null && currentProcess.HasExited == false) currentProcess.Kill();
+                            currentProcess = new System.Diagnostics.Process();
+                            currentProcess.StartInfo = new ProcessStartInfo("java.exe", "-Xmx600m -jar net.holdemresources.cli.jar " + getInputXMLname() + " " + getOutputXMLname()) { CreateNoWindow = true, UseShellExecute = false };
+                            currentProcess.SynchronizingObject = this;
+                            currentProcess.Exited += new EventHandler(CalcByCLI_Exited);
+                            currentProcess.EnableRaisingEvents = true;
+                            currentProcess.Start();
+                        }
+                    }
+
+                }
+                conn.Close();
+            }
+        }
+
+        private void readFromXML()
+        {
+            int heroCount = 0;
+            for (int i = 1, j = 0; i < 10; ++i)
+            {
+                if (chipTextBoxes[(bb_pos + i) % 9].Text.Trim() != string.Empty)
+                {
+                    if (SeatLabels[(bb_pos + i) % 9].Text == "H")
+                    {
+                        heroCount = j;
+                        break;
+                    }
+                    ++j;
+                }
+            }
+            for (int i = 1, j = 0; i < 10; ++i)
+            {
+                if (chipTextBoxes[(bb_pos + i) % 9].Text.Trim() != string.Empty)
+                {
+                    if (j < heroCount)
+                    {
+                        var spotPct = resultXML.SelectSingleNode(@"//spot[@pu=" + j.ToString() + "][@ca=" + heroCount.ToString() + "]/rangepct");
+                        var spotRange = resultXML.SelectSingleNode(@"//spot[@pu=" + j.ToString() + "][@ca=" + heroCount.ToString() + "]/range");
+                        rangeTextBoxes[(bb_pos + i) % 9].Text = spotPct.InnerText + " " + spotRange.InnerText;
+                        ++j;
+                    }
+                    else
+                    {
+                        var spotPct = resultXML.SelectSingleNode(@"//spot[@pu=" + heroCount.ToString() + "]/rangepct");
+                        var spotRange = resultXML.SelectSingleNode(@"//spot[@pu=" + heroCount.ToString() + "]/range");
+                        if (spotPct != null)
+                            rangeTextBoxes[(bb_pos + i) % 9].Text = spotPct.InnerText + " " + spotRange.InnerText;
+                        break;
+                    }
+                }
+            }
+            this.buttonCalc.Enabled = true;
+        }
+
+        private void CalcByCLI_Exited(object sender, EventArgs e)
+        {
+            var sr = new System.IO.StreamReader(getOutputXMLname());
+            string resultData = sr.ReadToEnd();
+            sr.Close();
+            System.IO.File.Delete(getInputXMLname());
+            System.IO.File.Delete(getOutputXMLname());
+
+            using (var conn = new SQLiteConnection("Data Source=calc.db"))
+            {
+                conn.Open();
+                using (var insertCommand = conn.CreateCommand())
+                {
+                    insertCommand.CommandText = "insert into CalculatedData values (@id, @data)";
+                    insertCommand.Parameters.Add("id", System.Data.DbType.String);
+                    insertCommand.Parameters.Add("data", System.Data.DbType.String);
+                    insertCommand.Prepare();
+                    insertCommand.Parameters["id"].Value = getSQLKey();
+                    insertCommand.Parameters["data"].Value = resultData;
+                    insertCommand.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+
+            resultXML = new XmlDocument();
+            resultXML.LoadXml(resultData);
+            readFromXML();
+        }
+
+        private void CalcByWeb()
+        {
             webBrowserTimer.Enabled = false;
 
             if (textBoxAnte.Text.Trim() == "")
@@ -30,7 +206,6 @@ namespace OpenNashCalculator
             URL += textBoxBB.Text;
             URL += "&sb=" + currentSB.Trim();
             URL += "&ante=" + textBoxAnte.Text.Trim();
-            textBoxStructure.Text = textBoxStructure.Text.Replace('+', ',').Replace(' ', ',');
             URL += "&structure=" + HttpUtility.UrlEncode(textBoxStructure.Text.Trim());
             for (int i = 1, j = 1; i < 10; ++i)
             {
@@ -62,7 +237,6 @@ namespace OpenNashCalculator
         {
             webBrowserTimer.Enabled = true;
         }
-
 
         private void webBrowserTimer_Tick(object sender, EventArgs e)
         {
